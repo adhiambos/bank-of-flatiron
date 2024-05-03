1ad1618a4d8b8c6dd86daa6563a0309a2d1cecd6
sharon adhiambo <adhiambosharonodhiambo@gmail.com>
2024-05-03 06:08:35 +0300

    add transactiontable

--- bank-of-flatiron/src/App.js
+++ bank-of-flatiron/src/App.js
@@ -1,21 +1,6 @@
 
-
-import './App.css';
-import AddTransaction from './components/AddTransaction';
-import Search from './components/Search';
-import Table from './components/Table';
-
-
-function App() {
-  return (
-    <div className="App">
-    <Search/>
-    <AddTransaction/>
-    <Table/>
-    
-     
-    </div>
-  );
+const App = () => {
+  return (  );
 }
-
+ 
 export default App;
