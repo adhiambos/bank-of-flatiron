030e27b0d1014af21f38589a3ac630c3a4083698
sharon adhiambo <adhiambosharonodhiambo@gmail.com>
2024-05-03 06:12:23 +0300

    update app

--- bank-of-flatiron/src/App.js
+++ bank-of-flatiron/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import TransactionTable from "./components/TransactionTable";
+import TransactionForm from "./components/TransactionForm";
 const App = () => {
   return ( 
     <div>
     <TransactionTable/>
+    <TransactionForm/>
     </div>
    );
 }
